854047776483e4382e049257aa39d25933b3b14f
Catfood Lab <274503048+hopeless-t@users.noreply.github.com>
2026-09-25 08:33:17 +0900

    code: expose reference attention helpers

---
+++ src/memory_attention_lab/attention/__init__.py
@@ -0,0 +1,17 @@
+from .reference import (
+    construct_values,
+    fold_memory_table,
+    headwise_rmsnorm,
+    inverse_rope_neox,
+    lookup_memory,
+    rope_neox,
+)
+
+__all__ = [
+    "construct_values",
+    "fold_memory_table",
+    "headwise_rmsnorm",
+    "inverse_rope_neox",
+    "lookup_memory",
+    "rope_neox",
+]
